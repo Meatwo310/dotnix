21dbde6f8c5235c9463b64924b44e630722240b9
Meatwo310 <git@meatwo310.net>
2026-03-28 00:56:50 +0900

    fix: let in構文なんもわからん

--- flake.nix
+++ flake.nix
@@ -7,24 +7,24 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, vscode-server, nixos-hardware, ... }@inputs: {
-    nixosConfigurations = {
-      sp9-v7 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          vscode-server.nixosModules.default
-          ./modules/common.nix
-          ./modules/desktop.nix
-          nixos-hardware.nixosModules.microsoft-surface-pro-9
-          ./hosts/sp9-v7/configuration.nix
-        ];
-      };
-    };
-
+  outputs = { self, nixpkgs, vscode-server, nixos-hardware, ... }@inputs:
     let
       pkgs = import nixpkgs { system = "x86_64-linux"; };
       surfaceCommon = nixos-hardware.nixosModules.microsoft-surface-common;
     in {
+      nixosConfigurations = {
+        sp9-v7 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            vscode-server.nixosModules.default
+            ./modules/common.nix
+            ./modules/desktop.nix
+            nixos-hardware.nixosModules.microsoft-surface-pro-9
+            ./hosts/sp9-v7/configuration.nix
+          ];
+        };
+      };
+
       # CI でビルドさせるパッケージを明示的に公開
       packages.x86_64-linux = {
         # longterm (LTS) カーネル
@@ -48,6 +48,4 @@
         #   }).config.boot.kernelPackages.kernel;
       };
     };
-  };
 }
-
