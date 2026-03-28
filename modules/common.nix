e12cac565f9ded47697c029ecd535352b34f28a9
Meatwo310 <git@meatwo310.net>
2026-03-28 15:20:56 +0900

    feat: nix-communityキャッシュを追加

--- modules/common.nix
+++ modules/common.nix
@@ -3,8 +3,14 @@
 {
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
-    substituters = [ "https://meatwo310-dotnix.cachix.org" ];
-    trusted-public-keys = [ "meatwo310-dotnix.cachix.org-1:F4Stc7Ivxgl72SHWe8z0pOHAe8Ip7zMFgOK6hdkh26k=" ];
+    substituters = [
+      "https://nix-community.cachix.org"      # priority 41
+      "https://meatwo310-dotnix.cachix.org"   # priority 42
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "meatwo310-dotnix.cachix.org-1:F4Stc7Ivxgl72SHWe8z0pOHAe8Ip7zMFgOK6hdkh26k="
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;
