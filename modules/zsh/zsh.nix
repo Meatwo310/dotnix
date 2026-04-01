327b7912a0b21be6951108b991527ee9a767a64c
Meatwo310 <git@meatwo310.net>
2026-04-01 20:48:02 +0900

    feat: omzとプラグインを少々

--- modules/zsh/zsh.nix
+++ modules/zsh/zsh.nix
@@ -10,6 +10,14 @@
     syntaxHighlighting.enable = true;
     shellInit = lib.mkBefore "source ${./.p10k.zsh}";
     promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "colored-man-pages"
+        "extract"
+      ];
+    };
   };
 
   system.userActivationScripts.zshrc = "touch .zshrc";
