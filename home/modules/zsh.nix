ddc982efaae2b99ad01657309a9d7e1de700322b
Meatwo310 <git@meatwo310.net>
2026-04-13 12:38:49 +0900

    fix: 古い記法をinitContentへ修正

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -7,11 +7,14 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    # p10k instant prompt must be sourced before everything else in .zshrc
-    initExtraFirst = "source ${./p10k.zsh}";
+    
+    initContent = lib.mkMerge [
+      # p10k instant prompt must be sourced before everything else in .zshrc
+      (lib.mkBefore "source ${./p10k.zsh}")
 
-    # Source p10k theme after oh-my-zsh
-    initExtra = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      # Source p10k theme after oh-my-zsh
+      "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
+    ];
 
     oh-my-zsh = {
       enable = true;
