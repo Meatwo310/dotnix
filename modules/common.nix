6372be4dac04c965b20bd6f53d60052b5296a267
Meatwo310 <git@meatwo310.net>
2026-07-26 16:05:37 +0900

    refactor(nix): extract shared base module

--- modules/common.nix
+++ modules/common.nix
@@ -1,21 +1,7 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 {
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    substituters = [
-      "https://nix-community.cachix.org"      # priority 41
-      "https://meatwo310-dotnix.cachix.org"   # priority 42
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "meatwo310-dotnix.cachix.org-1:F4Stc7Ivxgl72SHWe8z0pOHAe8Ip7zMFgOK6hdkh26k="
-    ];
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  time.timeZone = "Asia/Tokyo";
+  imports = [ ./base.nix ];
 
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
@@ -43,11 +29,6 @@
     extraGroups = [ "wheel" ];
   };
 
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-  };
-
   programs = {
     vim = {
       enable = true;
@@ -55,9 +36,4 @@
     };
     git.enable = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    delta
-    wget
-  ];
 }
