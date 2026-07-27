99fc6264d5fd1c73fe540b19758cfc1cfd6c6c93
Meatwo310 <git@meatwo310.net>
2026-07-27 13:46:23 +0900

    feat(sp9-v7): enable Japanese input

--- hosts/sp9-v7/configuration.nix
+++ hosts/sp9-v7/configuration.nix
@@ -17,6 +17,18 @@
 
   console.keyMap = "jp106";
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-mozc
+        kdePackages.fcitx5-qt
+      ];
+      waylandFrontend = true;
+    };
+  };
+
   networking.networkmanager.enable = true;
 
   environment.systemPackages = with pkgs; [
