9d97849202af8da1a4b28229f61841fe18fb47f4
Meatwo310 <git@meatwo310.net>
2026-07-30 10:44:44 +0900

    feat(plasma): manage global shortcuts

--- home/modules/plasma.nix
+++ home/modules/plasma.nix
@@ -57,7 +57,35 @@ in
     configFile.powerdevilrc."LowBattery/Display".UseProfileSpecificDisplayBrightness = false;
 
     shortcuts = {
-      kwin."Window Close" = [ "Meta+Q" "Alt+F4" ];
+      kwin = {
+        "Switch to Desktop 1" = "Meta+1";
+        "Switch to Desktop 2" = "Meta+2";
+        "Switch to Desktop 3" = "Meta+3";
+        "Switch to Desktop 4" = "Meta+4";
+        "Switch to Desktop 5" = "Meta+5";
+        "Switch to Desktop 6" = "Meta+6";
+        "Switch to Desktop 7" = "Meta+7";
+        "Switch to Desktop 8" = "Meta+8";
+        "Switch to Desktop 9" = "Meta+9";
+        "Window Close" = [ "Meta+Q" "Alt+F4" ];
+      };
+
+      plasmashell = {
+        "activate task manager entry 1" = [ ];
+        "activate task manager entry 2" = [ ];
+        "activate task manager entry 3" = [ ];
+        "activate task manager entry 4" = [ ];
+        "activate task manager entry 5" = [ ];
+        "activate task manager entry 6" = [ ];
+        "activate task manager entry 7" = [ ];
+        "activate task manager entry 8" = [ ];
+        "activate task manager entry 9" = [ ];
+        "manage activities" = [ ];
+        "next activity" = "Meta+A";
+        "previous activity" = "Meta+Shift+A";
+      };
+
+      yakuake."toggle-window-state" = "Zenkaku Hankaku";
     };
   };
 }
